0ea4a4cc81afff7e6a7b52b0b6744c668d80e998
dragonsky2357 <qkrdydals327@gmail.com>
2023-09-17 22:13:53 +0900

    feat: config env

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:daum_postcode_search/daum_postcode_search.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:frontend/src/account/state/AuthState.dart';
@@ -7,10 +8,11 @@ import 'package:frontend/src/account/screen/LoginScreen.dart';
 import 'package:frontend/src/main/screen/MainScreen.dart';
 import 'package:provider/provider.dart';
 import 'package:kpostal/kpostal.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await dotenv.load(fileName: 'assets/config/.env');
   await NaverMapSdk.instance.initialize(
       clientId: 'vbi897aazw',
       onAuthFailed: (error) {
@@ -29,6 +31,15 @@ class MyApp extends StatelessWidget {
       providers: [ChangeNotifierProvider(create: (_) => AuthState())],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('ko', 'KR'), // 한국어
+          Locale('en', 'US'), // 영어
+        ],
         title: 'My App',
         home: Main(),
       ),
