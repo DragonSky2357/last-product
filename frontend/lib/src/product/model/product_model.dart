0ad40e25d7e478256edda3d0967e66c732c0e5e3
dragonsky2357 <qkrdydals327@gmail.com>
2023-09-17 22:18:20 +0900

    refactor: produt modal

--- frontend/lib/src/product/model/product_model.dart
+++ frontend/lib/src/product/model/product_model.dart
@@ -6,44 +6,45 @@ class Product {
   final String id;
   final String title;
   final String description;
-  final int price;
+  final int fixedPrice;
+  final int salePrice;
+  final DateTime manufactureDate;
+  final DateTime expirationDate;
   final File? image;
-  final String address;
-  final double? latitude;
-  final double? longitude;
 
   Product({
     this.id = "",
     required this.title,
     required this.description,
-    required this.price,
+    required this.fixedPrice,
+    required this.salePrice,
+    required this.manufactureDate,
+    required this.expirationDate,
     this.image,
-    this.address = "",
-    this.latitude = 0.0,
-    this.longitude = 0.0,
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
-        id: json['id'],
-        title: json['title'],
-        description: json['description'],
-        price: json['price'],
-        image: json['image'],
-        address: json['address'],
-        latitude: json['latitude'],
-        longitude: json['longitude']);
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      image: json['image'],
+      fixedPrice: json['fixed_price'],
+      salePrice: json['sale_price'],
+      manufactureDate: json['manufacture_date'],
+      expirationDate: json['expiration_date'],
+    );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'title': title,
       'description': description,
-      'price': price,
       'image': image,
-      'address': address,
-      'latitude': latitude,
-      'longitude': longitude
+      'fixedPrice': fixedPrice,
+      'salePrice': salePrice,
+      'manufactureDate': manufactureDate,
+      'expirationDate': expirationDate
     };
   }
 }
